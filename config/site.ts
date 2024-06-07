07a098211c0a85fcc32792a688329f6998301f1c
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-06-08 03:46:23 +0530

    Add site config (contains constants)

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "Let Me Sleep",
+  url: "https://to-be-determined.com",
+  description: "Brain-Dump For Me, Blog For You",
+  author: "Bishwajeet Sahoo",
+  links: {
+    github: "https://github.com/SahooBishwajeet",
+    linkedin: "https://linkedin.com/in/bishwajeet-sahoo",
+    portfolio: "https://sahoobishwajeet.xyz",
+    twitter: "https://x.com/2004sahoobishwa",
+    discord: "https://discordapp.com/channels/@me/729644267856986114/",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
