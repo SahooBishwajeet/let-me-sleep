1f9d8145261d9b97698b825ae16228e92130d55a
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-06-08 01:57:49 +0530

    velite : compute slugs

--- velite.config.ts
+++ velite.config.ts
@@ -1,15 +1,22 @@
 import { describe } from "node:test";
 import { defineConfig, defineCollection, s } from "velite";
 
+const computedFields = <T extends { slug: string }>(data: T) => ({
+  ...data,
+  slugAsParams: data.slug.split("/").slice(1).join("/"),
+});
+
 const posts = defineCollection({
   name: "Post",
   pattern: "blog/**/*.mdx",
-  schema: s.object({
-    slug: s.path(),
-    title: s.string().max(99),
-    describtion: s.string().max(999).optional(),
-    date: s.isodate(),
-    published: s.boolean().default(true),
-    body: s.mdx(),
-  }),
+  schema: s
+    .object({
+      slug: s.path(),
+      title: s.string().max(99),
+      describtion: s.string().max(999).optional(),
+      date: s.isodate(),
+      published: s.boolean().default(true),
+      body: s.mdx(),
+    })
+    .transform(computedFields),
 });
