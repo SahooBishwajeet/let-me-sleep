d6adc4b2225bf48ef92228c8a6ac2d429c38eb79
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-06-08 03:48:09 +0530

    Add site header & nav component

---
+++ components/main-nav.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import Link from "next/link";
+import { Icons } from "./icons";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export function MainNav() {
+    const pathname = usePathname();
+
+    return(
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+            <Link href="/" className="mr-6 flex items-center space-x-2">
+                <Icons.logo className="w-6 h-6" />
+                <span className="font-bold">{ siteConfig.name }</span>
+            </Link>
+            <Link href="/dump" className={cn(
+                "text-sm font-medium transition-colors hover:text-primary",
+                pathname === "/dump" ? "text-foreground" : "text-foreground/60"
+                )}>
+                Dump
+            </Link>
+            <Link href="/about" className={cn(
+                "text-sm font-medium transition-colors hover:text-primary",
+                pathname === "/about" ? "text-foreground" : "text-foreground/60"
+                )}>
+                About
+            </Link>
+        </nav>
+    );
+}
---
+++ components/site-header.tsx
@@ -0,0 +1,70 @@
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { Icons } from "./icons";
+import { MainNav } from "./main-nav";
+
+export function SiteHeader() {
+    return(
+        <header className="sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+            <div className="container flex h-14 max-w-screen-2xl items-center">
+                <MainNav />
+                <div className="flex flex-1 items-center justify-end space-x-2">
+                    <nav className="fex items-center">
+                        <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+                            <div className={cn(
+                                buttonVariants({ variant: "ghost" }),
+                                "w-10 px-0"
+                            )}>
+                                <Icons.gitHub className="h-full w-full" />
+                                <span className="sr-only">GitHub</span>
+                            </div>
+                        </Link>
+                        
+                        <Link href={siteConfig.links.discord} target="_blank" rel="noreferrer">
+                            <div className={cn(
+                                buttonVariants({ variant: "ghost" }),
+                                "w-10 px-0"
+                            )}>
+                                <Icons.discord className="h-full w-full" />
+                                <span className="sr-only">Discord</span>
+                            </div>
+                        </Link>
+
+                        <Link href={siteConfig.links.linkedin} target="_blank" rel="noreferrer">
+                            <div className={cn(
+                                buttonVariants({ variant: "ghost" }),
+                                "w-10 px-0"
+                            )}>
+                                <Icons.linkedIn className="h-full w-full" />
+                                <span className="sr-only">LinkedIn</span>
+                            </div>
+                        </Link>
+
+                        <Link href={siteConfig.links.twitter} target="_blank" rel="noreferrer">
+                            <div className={cn(
+                                buttonVariants({ variant: "ghost" }),
+                                "w-10 px-0"
+                            )}>
+                                <Icons.twitter className="h-full w-full" />
+                                <span className="sr-only">Twitter</span>
+                            </div>
+                        </Link>
+
+                        <Link href={siteConfig.links.portfolio} target="_blank" rel="noreferrer">
+                            <div className={cn(
+                                buttonVariants({ variant: "ghost" }),
+                                "w-10 px-0"
+                            )}>
+                                <Icons.portfolio className="h-full w-full" />
+                                <span className="sr-only">Portfolio</span>
+                            </div>
+                        </Link>
+                        
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+}
