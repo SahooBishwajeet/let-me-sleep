70614f449d2798a3a346d23e6a216297746766f7
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-06-09 11:35:50 +0530

    Configure rehype pretty code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,7 +20,8 @@ const config = {
     },
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        // sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["Roboto Mono", ...fontFamily.sans],
       },
       colors: {
         border: "hsl(var(--border))",
--- velite.config.ts
+++ velite.config.ts
@@ -1,5 +1,8 @@
 import { describe } from "node:test";
 import { defineConfig, defineCollection, s } from "velite";
+import rehypeSlug from "rehype-slug";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 const computedFields = <T extends { slug: string }>(data: T) => ({
   ...data,
@@ -32,7 +35,20 @@ export default defineConfig({
   },
   collections: { posts },
   mdx: {
-    rehypePlugins: [],
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypePrettyCode, { theme: "github-dark" }],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          properties: {
+            className: ["subheading-anchor"],
+            ariaLabel: "Link to section",
+          },
+        },
+      ],
+    ],
     remarkPlugins: [],
   },
 });
