416bb4a2b29d080e66962ad483702a853726b03e
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2024-06-09 11:36:36 +0530

    Add rehype syntax highlighting and link headings

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-pt-[4rem]">
       <body className={cn(
           "min-h-screen bg-background font-sans antialiased",
           inter.variable
